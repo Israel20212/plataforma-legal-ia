21aec5615d2fccaf83ea080c2ba95b371cd11a56
Israel20212 <a3520110575@alumno.uttehuacan.edu.mx>
2025-08-12 12:43:57 -0600

    Fix: cargar CSS y JS con Vite + ajustes Tailwind v4

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'Plataforma Legal')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css'])
+
+    {{-- Carga ambos (CSS + JS) desde Vite --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-gray-100 font-sans">
 
     <!-- 🔵 NAVBAR -->
